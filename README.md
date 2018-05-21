9811702dc3910a5f88590e6817f83383e4d6cc93
vananhhoang <anhhtv.93@gmail.com>
2018-05-21 09:23:08 +0800

    added build execute jar file

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Amazon_Testing
 Perform automation test for Amazon website
+
+# Build project
+mvn clean install
+
+# Execute test
+
+java -jar target/amazon-0.0.1-SNAPSHOT.jar -c CONFIG_FILE_PATH  -g GROUP_OF_TESTCASE -o OUTPUT_TEST_REPORT_DIRECTORY
+
+Ex: java -jar target/amazon-0.0.1-SNAPSHOT.jar -c Resource/Configuration/config.properties  -g "Full" -o ouput
+	java -jar target/amazon-0.0.1-SNAPSHOT.jar -c Resource/Configuration/config.properties  -g "Smoke"
+
--- amazon/src/main/java/test/amazon/RunTest.java
+++ amazon/src/main/java/test/amazon/RunTest.java
@@ -2,6 +2,7 @@ package test.amazon;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.testng.TestNG;
@@ -17,12 +18,48 @@ import org.uncommons.reportng.JUnitXMLReporter;
  */
 public class RunTest 
 {
-    public static void main( String[] args )
-    {
+	private static String configPath = "Resource/Configuration/config.properties";
+	private static String group = "Smoke";
+	private static String output="test-ouput";
+	
+	private static void loadParameters(String[] args) throws Exception{
+		Map<String, String> params = new HashMap<String, String>();
+		for (int index = 0; index < args.length; index+=2) {
+			String key = args[index];
+			if (index+1 > args.length) {
+				throw new Exception("Missing value at " + key);
+			}
+			String value = args[index+1];
+			params.put(key, value);
+		}
+		for (Map.Entry<String, String> entry : params.entrySet()) {
+		    String key = entry.getKey();
+		    switch (key) {
+				case "--config":
+				case "-c":
+					RunTest.configPath = entry.getValue();
+					break;
+				case "--group":
+				case "-g":
+					RunTest.group = entry.getValue();
+					break;
+				case "--output":
+				case "-o":
+					RunTest.output = entry.getValue();
+					break;
+				default:
+					break;
+			}
+		}
+	}
+	
+    public static void main( String[] args ) throws Exception{
+    	RunTest.loadParameters(args);
+    	
         XmlSuite suite = new XmlSuite();
 		suite.setName("Amazon Suite");
 		Map<String, String> parameters = suite.getParameters();
-		parameters.put("configFile", "Resource/Configuration/config.properties");
+		parameters.put("configFile", configPath);
 		suite.setParameters(parameters);
 		
 
@@ -32,17 +69,16 @@ public class RunTest
 		xmltest.setXmlClasses(Arrays.asList(new XmlClass("test.amazon.testsuite.normal.LoginTest"),
 											new XmlClass("test.amazon.testsuite.normal.PurchaseOrderTest")));
 		
-		xmltest.setIncludedGroups(Arrays.asList("Smoke"));
+		xmltest.setIncludedGroups(Arrays.asList(group));
 		suite.setListeners(Arrays.asList(HTMLReporter.class.getName(), JUnitXMLReporter.class.getName()));
 		
-		String outDirPath = "test-output";
-		File outDir = new File(outDirPath);
+		File outDir = new File(output);
 		if(!outDir.exists()) {
 			outDir.mkdirs();
 		}
 		
 		TestNG tng = new TestNG();
-    	tng.setOutputDirectory(outDirPath);
+    	tng.setOutputDirectory(output);
     	tng.setUseDefaultListeners(false);
     	tng.setXmlSuites(Arrays.asList(suite));
     	tng.run();
